47974288255fdebe38fd67a9cf63aa82ab92f56a
LuukVesters <slxxpfn@gmail.com>
2021-12-16 14:39:08 +0100

    Upload

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+/*
+    Auteur:     Rob Wessels
+    Datum:      november 2020
+    
+    Onderwerp:  Responsive opdr. 3.2
+*/
+
+var menuItems = ["The Godfather", "Scarface", "Donnie Brasco", "Carlito's Way"];
+
+document.getElementById("menu_list").innerHTML = 
+        "<li><a href=\"#\">" + menuItems[0] + "</a></li>" +
+        "<li><a href=\"#\">" + menuItems[1] + "</a></li>" +
+        "<li><a href=\"#\">" + menuItems[2] + "</a></li>" +
+        "<li><a href=\"#\">" + menuItems[3] + "</a></li>" ;
+        
